f173e9079b0bfcb3228c6de50c13d4b02ba23617
user <ejrtn0852@naver.com>
2024-06-29 14:03:55 +0900

    update site

--- src/script/service/ImageSliderService.js
+++ src/script/service/ImageSliderService.js
@@ -6,7 +6,6 @@ import ImgSlider from "../event/ImgSlider.js";
 const ImageSliderService = async () => {
   const element = Element();
   const imgBinding = await StartBind();
-  console.log("이미지 바인딩 첫 호출: " + imgBinding)
   const message = await Message();
 
   const createIndex = imgBinding.imgSize();
@@ -14,7 +13,6 @@ const ImageSliderService = async () => {
   element.createBannerElement(createIndex);
   message.updateEventMessage(createIndex);
   imgBinding.bindImage();
-  console.log("이미지 바인딩 세 번째 호출: " + imgBinding.bindImage())
   const imgSlider = ImgSlider();
   imgSlider.addEventHandlers();
   await  imgSlider.start();
